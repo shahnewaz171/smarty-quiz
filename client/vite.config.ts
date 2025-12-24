e9c0d293b2d9c44ab9e9314f12df5a01fe87dab6
shahnewaz171 <shahnewaz601@gmail.com>
2025-12-24 14:53:33 +0600

    chore: added test script

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,11 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler']
+      }
+    }),
     tailwindcss(),
     visualizer({
       open: true,
